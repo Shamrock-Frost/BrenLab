02f3b74c4843bcbd51729c5ea7ea275710b74f45
Brendan Murphy <shamrockfrost@gmail.com>
2023-09-28 20:26:32 -0600

    Add hack in build to fix edit button on windows

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 opam exec -- forester build --dev --root bsm-0001 trees/
+for filename in output/*.xml; do
+gawk -i inplace -F '<source-path>' '
+NR <= 2 { print }
+NR >  2 { 
+    printf("%s", $1)
+    for(i = 2; i <= NF; i++) { 
+        s = substr($i, 8)
+        j = index(s, "<")
+        path = substr(s,1,j-1)
+        after = substr(s, j)
+        gsub("/","\\", path)
+        printf("<source-path>C:\\%s%s", path, after)
+    }
+}' $filename
+done
+
